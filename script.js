82703fdfb2d7be75088e985c289f8a686c253dea
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2023-08-11 14:12:14 -0300

    Criação do switch e das opções de cadastro

--- script.js
+++ script.js
@@ -3,12 +3,35 @@ let opçao = "";
 
 do {
   opçao = prompt(
-    "Bem-vindo ao Cadastro de imóveis" +
+    "Bem-vindo ao Cadastro de imóveis: " +
       imoveis.length +
       "\nEscolha uma das opções abaixo\n\n1. Novo imóvel\n2. Listar imóvel\n3. Sair"
   );
   switch (opçao) {
     case "1":
+      const imovel = {};
+      imovel.proprietario = prompt("Digite o nome do proprietario do imovel: ");
+      imovel.quartos = prompt(
+        "Digite a quantidade de quartos que o imóvel possui: "
+      );
+      imovel.banheiros = prompt(
+        "Digite a quantiadade de banheiros que o imóvel possui: "
+      );
+      imovel.garagem = prompt("O imóvel possui garagem ? (SIM/NÃO)");
+      const confirma = confirm(
+        "Salvar este imóvel ?\n" +
+          "\nProprietario: " +
+          imovel.proprietario +
+          "\nQuartos: " +
+          imovel.quartos +
+          "\nBanheiros: " +
+          imovel.banheiros +
+          "\nGaragem: " +
+          imovel.garagem
+      );
+      if (confirma) {
+        imoveis.push(imovel);
+      }
       break;
     case "2":
       break;
