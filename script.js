7356e16ef3d40cdc743abb766ebe424d703e6d2d
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2023-08-11 13:55:25 -0300

    Criação do loo

--- script.js
+++ script.js
@@ -2,5 +2,21 @@ const imoveis = [];
 let opçao = "";
 
 do {
-  prompt("Bem-vindo ao Cadastro de imóveis");
+  opçao = prompt(
+    "Bem-vindo ao Cadastro de imóveis" +
+      imoveis.length +
+      "\nEscolha uma das opções abaixo\n\n1. Novo imóvel\n2. Listar imóvel\n3. Sair"
+  );
+  switch (opçao) {
+    case "1":
+      break;
+    case "2":
+      break;
+    case "3":
+      alert("Encerrando cadastro");
+      break;
+    default:
+      alert("Opção invalida ! tente novamente");
+      break;
+  }
 } while (opçao !== "3");
